a418bfc41311cf1725b175ab152f6f0b672f4635
ccgamble <christine.c.gamble@gmail.com>
2016-12-09 09:13:04 -0700

    fixed requested changes

--- app/components/new-reminder.js
+++ app/components/new-reminder.js
@@ -12,8 +12,8 @@ export default Ember.Component.extend({
 
   actions: {
     createReminder() {
-      const reminder = this.getProperties('title', 'date', 'notes');
-			reminder.date = new Date(reminder.date);
+    	const reminder = this.getProperties('title', 'date', 'notes');
+      reminder.date = new Date(reminder.date);
       this.get('store').createRecord('reminder', reminder).save().then(() => {
         this.setProperties({ title: '', date: '', notes: '' });
       });
