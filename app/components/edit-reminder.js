23d0fe2e09dc6d04584503af038f3633deb4fc9d
Nick <nicknchambers@gmail.com>
2016-12-10 11:18:17 -0700

    wip routes to edit path and renders inputs for editing

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  store: Ember.inject.service(),
+
+  tagName: 'section',
+  classNames: ['add-reminder'],
+
+  title: '',
+  date: '',
+  notes: '',
+
+  actions: {
+    editReminder() {
+      const reminder = this.getProperties('title', 'date', 'notes');
+			reminder.date = new Date(reminder.date);
+      this.get('store').findRecord('reminder',  reminder).save().then(() => {
+        this.setProperties({ title: '', date: '', notes: '' });
+      });
+    }
+  }
 });
