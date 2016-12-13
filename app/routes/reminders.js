89fd00790f10840aeb47f0bbfd6b6c2cbc7d680c
Nick <nicknchambers@gmail.com>
2016-12-13 14:32:24 -0700

    adds delete functionality to individual reminder button

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		return this.get('store').findAll('reminder');
+	},
+	actions: {
+		handleRemove(model) {
+			console.log(model.id)
+			this.get('store').findRecord('reminder',  model.id).then((record) => {
+			 model.destroyRecord('reminder', model.id);
+		 });
+	 }
 	}
-
 });
